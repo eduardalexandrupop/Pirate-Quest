85196ad47c24ce1597afa01723df4aade1c14e55
Alexandru Bumbu <alexandru.bumbu@yahoo.com>
2020-04-09 20:04:31 +0100

    basic player movement + animation

---
+++ GameDev_Cw_2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    private float speed = 3.0f;
+    private Vector2 moveVector;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector.x = Input.GetAxisRaw("Horizontal");
+        moveVector.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("HorizontalMove", moveVector.x);
+        animator.SetFloat("VerticalMove", moveVector.y);
+        animator.SetFloat("Speed", moveVector.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVector.normalized * speed * Time.fixedDeltaTime);
+    }
+}
