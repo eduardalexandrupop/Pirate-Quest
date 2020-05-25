46f69af7605df8e35782e5650cd6c86268735543
Alexandru Bumbu <alexandru.bumbu@yahoo.com>
2020-05-25 21:28:29 +0100

    part1 arenas done

---
+++ GameDev_Cw_2/Assets/Scripts/BeesArenaManager.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BeesArenaManager : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject beePrefab;
+    public GameObject honeyPrefab;
+    public Image attackBar;
+    public Image[] lives;
+    public Text dead;
+    public Text timerText;
+    public Text counterText;
+
+    private List<Vector2> spawnPoints;
+    private List<Vector2> honeySpawnPoints;
+    private static GameObject playerInstance;
+
+    private float timeBetweenWaves = 7f;
+    private float arenaDuration = 60f;
+    private float timer;
+
+    private float startTime;
+
+    private static int honeyGathered;
+
+    private static int honeyOnMap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInstance = Instantiate(playerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        playerInstance.GetComponent<PlayerAttack>().attackBar = attackBar;
+        playerInstance.GetComponent<Player>().lives = lives;
+        playerInstance.GetComponent<Player>().dead = dead;
+
+        spawnPoints = new List<Vector2>();
+        spawnPoints.Add(new Vector2(8, 3));
+        spawnPoints.Add(new Vector2(-8, -3));
+        spawnPoints.Add(new Vector2(8, -3));
+        spawnPoints.Add(new Vector2(-8, 3));
+        spawnPoints.Add(new Vector2(6, 5));
+        spawnPoints.Add(new Vector2(-6, -5));
+        spawnPoints.Add(new Vector2(6, -5));
+        spawnPoints.Add(new Vector2(-6, 5));
+
+        honeySpawnPoints = new List<Vector2>();
+        honeySpawnPoints.Add(new Vector2(-5, -2));
+        honeySpawnPoints.Add(new Vector2(-5, 2));
+        honeySpawnPoints.Add(new Vector2(5, 2));
+        honeySpawnPoints.Add(new Vector2(5, -2));
+        honeySpawnPoints.Add(new Vector2(0, -2));
+        honeySpawnPoints.Add(new Vector2(-5, 2));
+
+        startTime = Time.time;
+        timer = arenaDuration;
+        InvokeRepeating("spawnEnemies", 4f, timeBetweenWaves);
+        InvokeRepeating("spawnHoney", 2f, 5f);
+
+        honeyGathered = 0;
+        honeyOnMap = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float timePassed = Time.time - startTime;
+        timer = arenaDuration - timePassed;
+        if (timer < 0)
+            timer = 0;
+
+        displayTimer();
+        displayCounter();
+    }
+
+    void spawnEnemies()
+    {
+        if (timer > 0)
+        {
+            Vector2 spawnPoint1 = spawnPoints[Random.Range(0, spawnPoints.Count)];
+            Vector2 spawnPoint2 = spawnPoints[Random.Range(0, spawnPoints.Count)];
+            while (spawnPoint2 == spawnPoint1)
+                spawnPoint2 = spawnPoints[Random.Range(0, spawnPoints.Count)];
+
+            GameObject enemy;
+            enemy = Instantiate(beePrefab, new Vector3(spawnPoint1.x, spawnPoint1.y, 0), Quaternion.identity);
+            enemy.GetComponent<EnemyAI>().target = playerInstance;
+            enemy.GetComponent<EnemyAttack>().player = playerInstance;
+
+            enemy = Instantiate(beePrefab, new Vector3(spawnPoint2.x, spawnPoint2.y, 0), Quaternion.identity);
+            enemy.GetComponent<EnemyAI>().target = playerInstance;
+            enemy.GetComponent<EnemyAttack>().player = playerInstance;
+        }
+    }
+
+    void spawnHoney()
+    {
+        if (honeyOnMap < 3)
+        {
+            Vector2 honeySpawnPoint = honeySpawnPoints[Random.Range(0, honeySpawnPoints.Count)];
+
+            GameObject honey = Instantiate(honeyPrefab, new Vector3(honeySpawnPoint.x, honeySpawnPoint.y, 0), Quaternion.identity);
+            honey.GetComponent<HoneyComb>().setPlayerInstance(playerInstance);
+            honeyOnMap++;
+        }
+    }
+
+    void displayTimer()
+    {
+        if (timerText != null)
+            timerText.text = ((int)timer / 60).ToString() + ":" + ((int)timer - (int)timer / 60).ToString("D2");
+    }
+
+    void displayCounter()
+    {
+        counterText.text = honeyGathered.ToString() + "/8";
+    }
+
+    public static void gatherHoney()
+    {
+        honeyGathered++;
+        honeyOnMap--;
+    }
+
+    public static GameObject getPlayerInstance()
+    {
+        return playerInstance;
+    }
+}
---
+++ GameDev_Cw_2/Assets/Scripts/HoneyComb.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoneyComb : MonoBehaviour
+{
+    public GameObject collectBarPrefab;
+
+    private Rigidbody2D rb;
+    private bool beingCollected;
+    private float collectRange = 0.8f;
+
+    private GameObject playerInstance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        beingCollected = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if ((playerInstance.transform.position - transform.position).magnitude < collectRange)
+        {
+            if (beingCollected == false)
+            {
+                beingCollected = true;
+                StartCoroutine(collectHoney());
+            }
+        }
+        else
+        {
+            beingCollected = false;
+        }
+    }
+
+    public IEnumerator collectHoney()
+    {
+        GameObject collectBarInstance = Instantiate(collectBarPrefab, transform.position + new Vector3(0f, 0.4f, 0f), Quaternion.identity);
+        collectBarInstance.transform.localScale = new Vector3(2f, 0.2f, 1f);
+        float i;
+        for (i = 0f; i <= 2.1f; i += 0.2f)
+        {
+            if (beingCollected == true)
+            {
+                collectBarInstance.transform.localScale = new Vector3(2 - i, 0.2f, 1f);
+                if (collectBarInstance.transform.localScale.x <= 0f)
+                {
+                    BeesArenaManager.gatherHoney();
+                    Destroy(gameObject);
+                }
+                yield return new WaitForSeconds(0.07f);
+            }
+            else
+            {
+                GameObject.Destroy(collectBarInstance);
+                break;
+            }
+        }
+    }
+
+    public void setPlayerInstance(GameObject player)
+    {
+        playerInstance = player;
+    }
+}
